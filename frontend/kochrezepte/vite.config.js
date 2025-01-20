5704121db46a6ef84efd445b67337974e0745ea3
Grabner Mathias <grabne200054@sr.htlweiz.at>
2025-01-20 10:11:18 +0100

    add dockerize

--- frontend/kochrezepte/vite.config.js
+++ frontend/kochrezepte/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+   
+  },
+});
